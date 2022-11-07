a17d11cf79cd52a7fbc89d45569df35820aeddb4
itsmekunati <itsmekunati@gmail.com>
2022-11-07 10:05:37 +0000

    adding project files

---
+++ src/main/java/com/ark/blogs/ArkblogsApplication.java
@@ -0,0 +1,14 @@
+package com.ark.blogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkblogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkblogsApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/ark/blogs/ArkblogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ark.blogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkblogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
